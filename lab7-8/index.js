93aa68e6969913137526d695a70b403126e6f71a
Wojciech Jurasz <w67239@student.wsiz.edu.pl>
2023-05-05 13:11:39 +0200

    1

--- lab7-8/index.js
+++ lab7-8/index.js
@@ -2,5 +2,48 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  form, reset();
 });
+
+const email = document.querySelector("#email");
+
+email.addEventListener("input",()=>{
+  console.log(validateEmail(email.value))
+  if (!validateEmail(email.value)){
+    email.setCustomValidity("Proszę podać poprawny adres e-mail.");
+  }
+  else{
+    email.setCustomValidity("")
+  }
+})
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+const password = document.querySelector("#password")
+//  walidacja hasła
+function validatePassword(password) {
+  // długosc hasłą 8
+  if (password.length < 8) {
+    return false;
+  }
+  //  co najmniej jedną dużą literę
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+  //  co najmniej jedną małą literę
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+  // co najmniej jedną cyfrę
+  if (!/\d/.test(password)) {
+    return false;
+  }
+  //  co najmniej jeden znak specjalny
+  if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+    return false;
+  }
+  else{
+  return true;
+  }
+}
